c709b1d104aee275c84723c5f4c8cc48f4b9bf10
nizam.shixmetov <nizam.shixmetov@vaps.az>
2025-12-19 16:53:57 +0400

    Refactor App component structure to include ChatWidget with proper layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import "./App.css";
 import HomePage from "./App/page.js";
+import "./App.css";
+import ChatWidget from "./Components/ChatWidget.js";
 
 function App() {
   return (
-    <>
+    <div className="relative">
       <HomePage />
-    </>
+
+      {/* Chat Widget */}
+      <div className="fixed bottom-6 right-6 z-50">
+        <ChatWidget />
+      </div>
+    </div>
   );
 }
 
