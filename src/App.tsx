b6423006871e10b8ecdfeee4f8960443a82b6302
Shikhmetov Nizam <134426138+NizamSixmetov@users.noreply.github.com>
2026-01-04 18:10:59 +0400

    Refactor App component to implement Lenis context for improved accessibility of Lenis instance

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import "./App.css";
 import { useLenis } from "./hooks/useLenis.js";
 import TawkToManager from "./Components/TawkToManager.js";
+import { createContext, useContext } from "react";
+import type Lenis from "lenis";
+
+// Context for Lenis
+const LenisContext = createContext<React.RefObject<Lenis | null> | null>(null);
+
+export const useLenisInstance = () => useContext(LenisContext);
 
 function App() {
-  useLenis();
+  const lenisRef = useLenis();
 
   return (
-    <div className="relative">
-      <TawkToManager />
-    </div>
+    <LenisContext.Provider value={lenisRef}>
+      <div className="relative">
+        <TawkToManager lenisRef={lenisRef} />
+      </div>
+    </LenisContext.Provider>
   );
 }
 
